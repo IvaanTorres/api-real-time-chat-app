a1b60d3261b6c58aa0ba8941b4bd60e3ee7987f9
Ivan Torres Garcia <ivantorresgarcia.02@gmail.com>
2022-03-21 23:22:02 +0100

    ci(#15): finish up the Docker config to make possible the mix develop between UI and API

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,15 @@ import { config } from 'dotenv'
 
 config()
 
+// MongoDB config
 export const {
-  DB_CONNECTION, DB_HOST, DB_PORT, DB_DATABASE,
+  DB_CONNECTION,
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
 } = process.env
 
-export const { PORT } = process.env
+// Express config
+export const {
+  PORT,
+} = process.env
